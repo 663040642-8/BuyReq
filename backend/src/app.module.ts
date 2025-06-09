ca27cef969ada0514e9bceaaecca97df132a6ec1
Kittikan <kittikan.pi@kkumail.com>
2025-06-09 16:02:40 +0700

    add module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ShopsModule } from './shops/shops.module';
+import { ItemsToSellModule } from './items-to-sell/items-to-sell.module';
+import { OrdersModule } from './orders/orders.module';
+import { ChatModule } from './chat/chat.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'database.sqlite',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  }), UsersModule, AuthModule, ShopsModule, ItemsToSellModule, OrdersModule, ChatModule, ReviewsModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
